f81fd443b8bfc739364b9f6d513e2824bf852df5
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2018-06-28 00:22:56 +0200

    Finish Map Fragment

--- app/src/main/java/eg/edu/cu/fci/ecampus/fci_e_campus/fragments/MapFragment.java
+++ app/src/main/java/eg/edu/cu/fci/ecampus/fci_e_campus/fragments/MapFragment.java
@@ -1,16 +1,25 @@
 package eg.edu.cu.fci.ecampus.fci_e_campus.fragments;
 
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.github.chrisbanes.photoview.PhotoView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import eg.edu.cu.fci.ecampus.fci_e_campus.R;
 
 public class MapFragment extends Fragment {
+
+    private static final String TAG = MapFragment.class.getSimpleName();
 
     public MapFragment() {
         // Required empty public constructor
@@ -36,13 +45,32 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        PhotoView mapPhotoView = fragmentView.findViewById(R.id.pv_map);
+
+        // load image from assets
+        try {
+            // get input stream
+            InputStream facultyMapInputStream = getContext().getAssets().open("faculty_map.png");
+            // load image as Drawable
+            Drawable facultyMapDrawable = Drawable.createFromStream(facultyMapInputStream, null);
+            // set image to PhotoView
+            mapPhotoView.setImageDrawable(facultyMapDrawable);
+        }
+        catch (IOException e) {
+            Log.d(TAG, e.toString());
+
+            Toast.makeText(getContext(), "Error loading map!", Toast.LENGTH_SHORT).show();
+        }
+
+        return fragmentView;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onStart() {
+        super.onStart();
 
-
+        Toast.makeText(getContext(), "Hint: use pinch to zoom.", Toast.LENGTH_SHORT).show();
     }
 }
